1c8cb74d0cdffc3681d88fa0d4e809ed401bb00a
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-12-25 13:53:53 +0200

    Exercise 10.3

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+
+const RepositoryItem = (props) => {
+  return (
+    <View>
+      <Text>Full name: {props.fullName}</Text>;
+      <Text>Description: {props.description}</Text>
+      <Text>Language: {props.language}</Text>
+      <Text>Stars: {props.stargazersCount}</Text>
+      <Text>Forks: {props.forksCount}</Text>
+      <Text>Reviewes: {props.reviewCount}</Text>
+      <Text>Rating: {props.ratingAverage}</Text>
+    </View>
+  )
+
+};
+
+export default RepositoryItem;
